183c9974c8299bc7ca5f034731c7cc315219796c
srimanthvasireddy <srimanth.vasireddy@onehubpos.com>
2025-06-22 08:07:20 +0530

    Added sync Immediate After Install an Application

--- app/src/main/java/com/dev/storeapp/StoreApp.kt
+++ app/src/main/java/com/dev/storeapp/StoreApp.kt
@@ -5,6 +5,9 @@ import com.dev.storeapp.app.utils.AppLogger
 import dagger.hilt.android.HiltAndroidApp
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.dev.storeapp.app.worker.MasterDataSyncWorker
 import javax.inject.Inject
 import com.google.firebase.FirebaseApp
 
@@ -17,6 +20,7 @@ class StoreApp : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        syncImmediately()
         AppLogger.d("StoreApp", "Application started")
     }
 
@@ -24,4 +28,18 @@ class StoreApp : Application(), Configuration.Provider {
         get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
+
+
+    private fun syncImmediately(){
+        val constraints = androidx.work.Constraints.Builder()
+            .setRequiredNetworkType(androidx.work.NetworkType.CONNECTED)
+            .build()
+
+        val workRequest = OneTimeWorkRequestBuilder<MasterDataSyncWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(workRequest)
+        AppLogger.e("AlarmReceiver", "Worker Started...")
+    }
 }

f8fac11296389eb668fd281b26d35c44532de188
srimanthvasireddy <srimanth.vasireddy@onehubpos.com>
2025-06-27 11:01:34 +0530

    created New Common Bottom Sheet Frag and Updated packages and Users Data

--- app/src/main/java/com/dev/storeapp/presentation/ui/dialog/AddUserDialogFragment.kt
+++ app/src/main/java/com/dev/storeapp/presentation/ui/dialog/AddUserDialogFragment.kt
@@ -1,22 +1,18 @@
-package com.dev.storeapp.presentation.ui.users
+package com.dev.storeapp.presentation.ui.dialog
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
-import com.dev.storeapp.R
 import com.dev.storeapp.app.base.BaseDialogFragment
 import com.dev.storeapp.data.local.entity.Address
 import com.dev.storeapp.data.local.entity.Name
 import com.dev.storeapp.data.local.entity.UserEntity
 import com.dev.storeapp.databinding.FragmentAddUserDialogBinding
-import com.dev.storeapp.presentation.ui.products.ProductsViewModel
+import com.dev.storeapp.presentation.ui.you.UsersViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class AddUserDialogFragment : BaseDialogFragment<FragmentAddUserDialogBinding>() {
